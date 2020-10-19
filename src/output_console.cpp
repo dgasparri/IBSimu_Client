b6f631ee522b98d3f80562466945e70a97bd671f
dgasparri <553197+dgasparri@users.noreply.github.com>
2020-10-19 14:34:29 +0200

    Cleaned console

--- src/output_console.cpp
+++ src/output_console.cpp
@@ -3,17 +3,8 @@
 
 namespace ic_output_console = ibsimu_client::output_console;
 
-/*
-void ic_output_console::output_console_options_m(
-    bpo::options_description &command_line_options_o)
-{
-        command_line_options_o.add_options()
-            ("display-console", bpo::value<bool>(), "Display console at end, set false for batch simulation")
-        ;
 
-
-}
-*/
+// See GTKPlotter for other plots
 
 ic_output_console::output_console_m_t ic_output_console::output_console_factory_m(
     const bool display_console,
@@ -46,57 +37,3 @@ ic_output_console::output_console_m_t ic_output_console::output_console_factory_
     
 
 }
-
-void particle_console()
-{
-
-    //read gtkparticlediagdialog.cpp GTKParticleDiagDialog::run
-
-    particle_diag_plot_type_e type = PARTICLE_DIAG_PLOT_NONE;
-    // PARTICLE_DIAG_PLOT_HISTO2D
-    // PARTICLE_DIAG_PLOT_HISTO1D
-    // PARTICLE_DIAG_PLOT_SCATTER
-
-    
-	coordinate_axis_e axis;
-    //AXIS_X;
-    //AXIS_R;
-    // AXIS_Y;
-    //AXIS_Z;
-	
-
-	// Read level
-	double level;
-	//const char *entry_text = gtk_entry_get_text( GTK_ENTRY(entry_level) );
-	//level = atof( entry_text );
-	
-
-	// Read plot type and set diagnostic accordingly
-	trajectory_diagnostic_e diagx = DIAG_NONE, 
-            diagy = DIAG_NONE;
-    //diagx = DIAG_X;
-	//diagy = DIAG_XP;
-    //diagx = DIAG_R;
-	//diagy = DIAG_RP;
-    //diagx = DIAG_Y;
-	//diagy = DIAG_YP;
-    //diagx = DIAG_R;
-	//diagy = DIAG_AP;
-    //diagx = DIAG_Z;
-	//diagy = DIAG_ZP;
-	    //diagx = DIAG_EK;
-        //diagx = DIAG_QM;
-        //diagx = DIAG_MASS;
-	    //diagx = DIAG_CHARGE;
-
-    //axis
-    //type
-    //level
-    //type
-    //diagx
-    //diagy
-
-    //_plotter->new_particle_plot_window( axis, level, type, diagx, diagy );
-}
-
-
